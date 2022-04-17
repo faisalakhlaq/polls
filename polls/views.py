e3c97271baf004931a9c0171aa242c669d71723b
Faisal <faisal.akhlaq@aol.com>
2022-04-17 21:37:15 +0200

    added polls views.

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
--- pollsapi/urls.py
+++ pollsapi/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/voters/", include("voters.urls")),
+    path("api/polls/", include("polls.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
