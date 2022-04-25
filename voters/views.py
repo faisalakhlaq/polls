3852267fbd1bd2a0087c2e9bb1a32e9bb19afd0a
Faisal <faisal.akhlaq@aol.com>
2022-04-25 16:02:43 +0200

    added github CD

--- voters/views.py
+++ voters/views.py
@@ -9,3 +9,13 @@ class VoterViewSet(ModelViewSet):
     queryset = Voter.objects.all()
     serializer_class = VoterSerializer
     permission_classes = (AllowAny,)
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == "list":
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
